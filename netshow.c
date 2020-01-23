c0ebf1c744a3b285ddf41c3ff2ef17330071be80
surdidi1 <60221223+surdidi1@users.noreply.github.com>
2020-01-23 15:17:39 +0100

    ww

--- netshow.c
+++ netshow.c
@@ -6,60 +6,17 @@
 #include <ifaddrs.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define ADDR_MAX 32
 
 
-int main(int argc, char *ifname[])
+int
+main(void)
 {
-        int i;
-        char *vide="x";
-        if (argc <= 1) {
-                printf("Le programme n'a reçu aucun argument\n");
-                ifname[1]="x";
-        }
-        else{
-                printf("Vous avez saisie : ");
-                for (i = 1; i < argc; i++) {
-                printf("%s\n\n", ifname[i]);
-                }
-        }
-
         static char buf[ADDR_MAX];
         struct ifaddrs *list;
         struct ifaddrs *it;
 
-        printf("ADRESSES EN IPV6 :\n");
-        if (getifaddrs(&list) != 0) {
-                perror("getifaddrs");
-                return EXIT_FAILURE;
-        }
-        it = list;
-        //printf('%s',it);
-        while (it != NULL) {
-                struct sockaddr_in *addr;
-
-                        addr = (struct sockaddr_in *)it->ifa_addr;     
-                        if (addr != NULL && it->ifa_addr->sa_family == AF_INET6) {
-                                if (inet_ntop(AF_INET6, &addr->sin_addr, buf, ADDR_MAX) == NULL) {
-                                        perror("inet_ntop");
-                                        exit(EXIT_FAILURE);
-                                }
-                                if(strcmp(ifname[1],vide)==0){
-                                        printf("%s: %s\n", it->ifa_name, buf);
-
-                                }else if (strcmp(ifname[1],it->ifa_name)==0){
-                                        printf("%s: %s\n", it->ifa_name, buf);
-                                }
-                                else{;}                
-                                
-                        }
-                it = it->ifa_next;
-        }
-        freeifaddrs(list);
-
-        printf("\nADRESSES EN IPV4 : \n");
         if (getifaddrs(&list) != 0) {
                 perror("getifaddrs");
                 return EXIT_FAILURE;
@@ -74,12 +31,7 @@ int main(int argc, char *ifname[])
                                 perror("inet_ntop");
                                 exit(EXIT_FAILURE);
                         }
-                        if(strcmp(ifname[1],vide)==0){
-                                printf("%s: %s\n", it->ifa_name, buf);
-
-                        }else if (strcmp(ifname[1],it->ifa_name)==0){
-                                printf("%s: %s\n", it->ifa_name, buf);
-                        }else{;}  
+                        printf("%s: %s\n", it->ifa_name, buf);
                 }
                 it = it->ifa_next;
         }
